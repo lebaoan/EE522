84d242e091fff39e0433a07e580308cbaf654c5b
lebaoan <lebao000@uw.edu>
2019-03-09 12:50:45 -0800

    Add files via upload

---
+++ blink.c
@@ -0,0 +1,39 @@
+// blink.c
+//
+// Example program for bcm2835 library
+// Blinks a pin on an off every 0.5 secs
+//
+// After installing bcm2835, you can build this 
+// with something like:
+// gcc -o blink blink.c -l bcm2835
+// sudo ./blink
+
+#include <bcm2835.h>
+#include <stdio.h>
+// Blinks on RPi Plug P1 pin 11 (which is GPIO pin 17)
+#define PIN RPI_GPIO_P1_11
+int main(int argc, char **argv)
+{
+
+    if (!bcm2835_init())
+      return 1;
+    // Set the pin to be an output
+    bcm2835_gpio_fsel(PIN, BCM2835_GPIO_FSEL_OUTP);
+    // Blink
+    while (1)
+    {
+        // Turn it on
+        bcm2835_gpio_write(PIN, HIGH);
+        
+        // wait a bit
+        bcm2835_delay(500);
+        
+        // turn it off
+        bcm2835_gpio_write(PIN, LOW);
+        
+        // wait a bit
+        bcm2835_delay(500);
+    }
+    bcm2835_close();
+    return 0;
+}
